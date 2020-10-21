2a48004766b32297c7a5154888bddcdfec9a78ab
zshengli <shengliz@cmu.andrew.edu>
2020-10-20 23:52:55 -0400

    code and writeup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,178 @@
-import streamlit as st
 import pandas as pd
-import altair as alt
+import streamlit as st
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier 
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score 
+import pickle 
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+@st.cache
+def get_airbnb_data():
+    return pd.read_csv("http://data.insideairbnb.com/united-states/ca/los-angeles/2020-09-09/visualisations/listings.csv")
 
-@st.cache  # add caching so we load the data only once
-def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
+df = get_airbnb_data()
+df['number_of_reviews'] = df['number_of_reviews'].astype('int64')
+st.title("IDS HW3")
+st.header("Dataset")
+st.subheader("Airbnb LA Dataset Overview")
+st.markdown("In this project, we are going to use the Airbnb dataset containing information of LA listings from .")
+st.markdown("Let's first take a look at the first few records of the dataset to get an overview.")
+st.dataframe(df.head())
 
-df = load_data()
+if st.checkbox('Show the whole dataset (There are 31536 rows, so it may take a while to run)'):
+    st.dataframe(df)
+    
+st.subheader("Selecting the most useful features")
+st.write(f"Out of all 16 features, the following ones are the most informative and useful features for our exploration tasks. Users could also add/delete features according to their needs. We will display the first 10 rows in our dataset.")
+cols_ofinterest = ["neighbourhood", "neighbourhood_group", "room_type", "price", "number_of_reviews", "availability_365", "reviews_per_month", "minimum_nights", 'calculated_host_listings_count', 'latitude', 'longitude']
+cols = st.multiselect("Columns", df.columns.tolist(), default=cols_ofinterest)
+st.dataframe(df[cols].head(10))
+df = df[cols]
 
-st.write("Let's look at raw data in the Pandas Data Frame.")
+st.header("Discoveries and Insights")
+st.subheader("What is the distribution of price?")
+st.write("Let's first take a look at the distribution of the main variable of our interest -- price. By looking at the distribution of price, one could gain a sense of the general price level of the LA listings. Users could also select a custom range using the sidebar ('Price range') on the left to adjust the histogram below. The default range we set is from $0 to $200. ")
+price_range = st.sidebar.slider("Price range", float(df.price.min()), float(df.price.clip(upper=1000.).max()), (0., 200.))
+min_hist_value_price = price_range[0]
+max_hist_value_price = price_range[1]
+f = px.histogram(df.query('price <= @max_hist_value_price and price >= @min_hist_value_price'), x="price", nbins=15, title="Price distribution")
+f.update_xaxes(title="Price")
+f.update_yaxes(title="# of listings")
+st.plotly_chart(f)
 
-st.write(df)
 
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
+st.subheader("What is the distribution of number of reviews?")
+st.write("Then let's take a look at the distribution of the number_of_reviews variable. Usually the more reviews a listing has, the more legit/popular is the listing. Users could also select a custom range using the sidebar ('Number of reviews range') on the left to adjust the histogram below. The default range we set is from 0 to 40. ")
+review_range = st.sidebar.slider("Number of reviews range", int(df.number_of_reviews.min()), int(df.number_of_reviews.clip(upper=100).max()), (0, 40))
+min_hist_value_numreviews = review_range[0]
+max_hist_value_numreviews = review_range[1]
+f = px.histogram(df.query('number_of_reviews <= @max_hist_value_numreviews and number_of_reviews >= @min_hist_value_numreviews'), x="number_of_reviews", nbins=15, title="Number of reviews distribution")
+f.update_xaxes(title="Number of reviews")
+f.update_yaxes(title="# of listings")
+st.plotly_chart(f)
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
-).properties(
-    width=600, height=400
-).interactive()
+st.subheader("What is the distribution of room type?")
+st.write("Then let's take a look at the distribution of room types using a pie chart.")
+df_room = df.groupby('room_type').count().reset_index()
+f = px.pie(df, values=df_room['price'], names= df_room['room_type'], title="Pie Chart of Room Type", color = df_room['room_type'])
+f.update_xaxes(title="Room Type")
+f.update_yaxes(title="#. of listings")
+st.plotly_chart(f)
+st.markdown("We can see that entire home/apt and private room constitute the majority of all the listings.")
 
-st.write(chart)
+st.subheader("Filter properties using number of reviews")
+st.write("We added an interative table below to enable users to set a range using the sideboxs to view the properties whose number of reviews fall in that range. The table below shows the top 100 properties and you can click the checkbox below to get all properties in that number of reviews range. The default values we set for the lower bound and the upper bound are 10 and 15 respectively. The listings displayed are sorted in descending order by number of reviews. ")
+numreview_min = st.sidebar.number_input("number of reviews lower bound", min_value=0, value = 10)
+numreview_max = st.sidebar.number_input("number of reviews upper bound", min_value=0, value = 15)
+if numreview_min > numreview_max:
+    st.error("This is not a valid range")
+else:
+    df.query("@numreview_min<=number_of_reviews<=@numreview_max").sort_values("number_of_reviews", ascending=False)\
+        .head(100)[["neighbourhood", "neighbourhood_group", "room_type", "price", "number_of_reviews", "availability_365", "reviews_per_month", "minimum_nights", 'calculated_host_listings_count']]
+    if st.checkbox('Click to show the whole list of properties with number of reviews in that range'):
+        df.query("@numreview_min<=number_of_reviews<=@numreview_max").sort_values("number_of_reviews", ascending=False)[["neighbourhood", "neighbourhood_group", "room_type", "price", "number_of_reviews", "availability_365", "reviews_per_month", "minimum_nights", 'calculated_host_listings_count']]
+
+st.subheader("Filter properties using price")
+st.write("We added an interative table below to enable users to set a range using the sideboxs to view the properties whose prices fall in that range. The table below shows the top 100 properties and you can click the checkbox below to get all properties in that price range. The default values we set for the lower bound and the upper bound are $50 and $200 respectively. The listings displayed are sorted in ascending order by price.")
+price_min = st.sidebar.number_input("price lower bound", min_value=0, value = 50)
+price_max = st.sidebar.number_input("price upper bound", min_value=0, value = 200)
+if price_min > price_max:
+    st.error("This is not a valid range")
+else:
+    df.query("@price_min<=price<=@price_max").sort_values("price", ascending=True)\
+        .head(100)[["neighbourhood", "neighbourhood_group", "room_type", "price", "number_of_reviews", "availability_365", "reviews_per_month", "minimum_nights", 'calculated_host_listings_count']]
+    if st.checkbox('Click to show the whole list of properties with price in that range'):
+        df.query("@price_min<=price<=@price_max").sort_values("price", ascending=True)\
+        [["neighbourhood", "neighbourhood_group", "room_type", "price", "number_of_reviews", "availability_365", "reviews_per_month", "minimum_nights", 'calculated_host_listings_count']]
+
+
+st.subheader("What is the average listing price of each neighbourhood?")
+st.markdown("If users would like to get a sense of the average listing prices by neighborhood, they could check the checkbox below.")
+if st.checkbox('Show the whole list of average listing prices by neighbourhood'):
+    st.table(df.groupby("neighbourhood").price.mean().reset_index()\
+        .round(2).sort_values("price", ascending=False)\
+        .assign(avg_price=lambda x: x.pop("price").apply(lambda y: "%.2f" % y)))
+
+st.markdown("The following table displays the average prices of the top 5 most expensive neighbourhoods")
+st.table(df.groupby("neighbourhood").price.mean().reset_index()\
+        .round(2).sort_values("price", ascending=False).head(5))
+
+st.markdown("Following are the average prices of the top 5 cheapest neighbourhoods")
+st.table(df.groupby("neighbourhood").price.mean().reset_index()\
+        .round(2).sort_values("price", ascending = True).head(5))
+
+st.subheader("what is the average listing price for each room type?")
+st.table(df.groupby("room_type").price.mean().reset_index()\
+    .round(2).sort_values("price", ascending=False)\
+    .assign(avg_price=lambda x: x.pop("price").apply(lambda y: "%.2f" % y)))
+
+f = px.bar(df.groupby(["room_type"]).price.mean().reset_index(), x="room_type", y='price', title="Price of each room type for Airbnb listings in LA", color = 'room_type')
+f.update_xaxes(title="Room Type")
+f.update_yaxes(title="Price")
+st.plotly_chart(f)
+
+st.markdown("We can see from the table and bar plot above that entire home/apt has the highest average price and shared room has the lowest average price.")
+
+st.subheader("Where are the most expensive properties located in LA?")
+st.markdown("The following map shows the most expensive Airbnbs with prices at $800 and above.")
+st.map(df.query("price>=800")[["latitude", "longitude"]].dropna(how="any"),)
+st.markdown("From the map, we can see that expensive properties are mainly concentrated at West Hollywood and Beverly Hills while some of them are scattered around the coastline.")
+st.markdown("Following are the top five most expensive Airbnb listings in LA.")
+st.write(df.query("price>=800").sort_values("price", ascending=False).head())
+st.markdown("We can notice that the top 5 most expensive listings are all entire home/apartment, which is the room type with the highest average price (shown previously).")
+
+st.subheader("Where are the cheapest properties located in LA?")
+st.markdown("The following map shows the cheapest Airbnbs priced at $50 and below.")
+st.map(df.query("price<=50")[["latitude", "longitude"]].dropna(how="any"))
+st.markdown("From the map, we can see that cheap properties are roughly evenly distributed in all locations in LA, while some of them belong to Santa Clarita and Palmdale.")
+st.markdown("Following are the top five cheapest Airbnb listings in LA.")
+st.write(df.query("price<=50").sort_values("price", ascending=True).head())
+st.markdown("From the table above, an interesting thing to notice is that all of the five listings are priced at $0 and they all don't have any reviews, indicating that either the data displayed above are inaccurate or those properties are not very legit.")
+
+st.subheader("Where are the most popular properties located in LA?")
+st.markdown("The following map shows the most popular Airbnbs with number of reviews at 100 and above.")
+st.map(df.query("number_of_reviews>=100")[["latitude", "longitude"]].dropna(how="any"),)
+st.markdown("Following are the top five most popular Airbnb listings in LA.")
+st.write(df.query("number_of_reviews>=100").sort_values("price", ascending=False).head())
+
+st.subheader("Where are the least popular properties located in LA?")
+st.markdown("The following map shows the most least popular Airbnbs with number of reviews at 5 and below.")
+st.map(df.query("number_of_reviews<=5")[["latitude", "longitude"]].dropna(how="any"),)
+st.markdown("Following are the top five least popular Airbnb listings in LA.")
+st.write(df.query("number_of_reviews<=5").sort_values("price", ascending=False).head())
+
+st.markdown("From the two maps above, we can see that the distribution of the most popular properties on map is similar to that of the most expensive properties. Also, the distribution of the least popular properties on map is similar to that of the cheapest properties. This indicates that it is likely that price is positively associated with number of reviews.")
+
+df = df.dropna()
+X = df[['number_of_reviews', 'availability_365', 'minimum_nights', 'calculated_host_listings_count']]
+X = X.apply(pd.to_numeric)
+y = df['price']
+y = y.apply(pd.to_numeric)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0) 
+classifier = LinearRegression() 
+classifier.fit(X_train, y_train) 
+y_pred = classifier.predict(X_test)
+
+def prediction(number_of_reviews, availability_365, minimum_nights, calculated_host_listings_count):      
+    prediction = classifier.predict( 
+        [[number_of_reviews, availability_365, minimum_nights, calculated_host_listings_count]]) 
+    print(prediction) 
+    return prediction[0]
+
+st.header("Listing Price Estimation")   
+st.markdown("What's shown below is the coolest part of our project! Users could get an estimation of the price after they specify the values for the following numerical features: number of reviews, availability, minimum_nights and host listings count. We leveraged the linear regression model to do the prediction.") 
+number_of_reviews = st.text_input("number_of_reviews (number of reviews for this property): ", "Type Here") 
+availability_365 = st.text_input("availability_365 (number of available days of this property in one year): ", "Type Here") 
+minimum_nights = st.text_input("minimum_nights (minimum number of nights required): ", "Type Here") 
+calculated_host_listings_count = st.text_input("calculated_host_listings_count (number of properties this host have on airbnb): ", "Type Here") 
+result ="" 
+
+if st.button("Show estimated price"): 
+    result = prediction(int(number_of_reviews), int(availability_365), int(minimum_nights), int(calculated_host_listings_count))
+    st.success('Based on given information, the estimated price is ${:.2f}.'.format(result))
+
+st.markdown("Now we've reached the end of our project. If you decide to visit LA some day and need information on selecting airbnbs, hopefully our project can be a great help!")
+btn = st.button("Thank you:)")
+if btn:
+    st.balloons()
--- writeup.md
+++ writeup.md
@@ -1,17 +1,75 @@
-# Project name
+# Project name: LA Airbnb Pricing
 
 ![A screenshot of your application. Could be a GIF.](screenshot.png)
 
-TODO: Short abstract describing the main goals and how you achieved them.
+
+This project used the Airbnb dataset which contains LA listings. Our main goal is to gain an overview of the Airbnb listings in LA, especially their prices, number of reviews etc. and be able to get an estimation of price given some key information.
 
 ## Project Goals
 
-TODO: **A clear description of the goals of your project.** Describe the question that you are enabling a user to answer. The question should be compelling and the solution should be focused on helping users achieve their goals. 
+Our project is designed to answer the following questions, which are proposed mainly from the perspective of Airbnb customers.     
+- What are the distributions of price, number of reviews and room type of the Airbnbs located in LA?
+- What are the most expensive neighboorhoods and the cheapest neighboorhouds?
+- What are the most expensive room type and the least expensive room type?
+- Where are the most and least popular/legit properties located in Los Angeles on the map?
+- Where are the most expensive and cheapest properties located in Los Angeles on the map?
+- What is the price estimation given number of reviews, availability in one year, minimum nights required, and host listings counts?
 
 ## Design
 
-TODO: **A rationale for your design decisions.** How did you choose your particular visual encodings and interaction techniques? What alternatives did you consider and how did you arrive at your ultimate choices?
+- Airbnb LA Listings
+
+    Most of the users of Airbnb are interested in the price. So we chose it to be the main variable of interest in our project. Then, we took a closer look at the dataset which contains in total of 16 variables. Among these variables, we selected a subset of columns which are able to sufficiently answer our research questions. These variables are neighbourhood_group, neighbourhood, latitude, longitude, room_type, price, minimum_nights, number of reviews, reviews_per_month, calculated_host_listings_count, availability_365). We generated a table with these key variables from our dataset. Users are also allowed to choose the variables they are interested in by themselves.
+
+    We included this interactive table, because we think it would be good for the users to select the columns that they think are more important according to their personal needs.
+
+- Distribution of variables
+
+    We plotted out the distributions of the main variables we are interested in. To make it more interactive, we added sidebars to each of the histograms which allow users to select a custom range of their interests. To determine the optimal range of the sidebar, we first select the range that covers all the values, then we clipped the upper bound to remove the outliers.
+
+    We added interaction techniques to these plots as we think it would be good for the user to select ranges by themselves, which enables them so to see the distributions more clearly in a smaller range.
+
+- What are the most expensive and cheapest neighbourhoods？What are the most expensive room type and the least expensive one?
+
+    Then We are curious about which neighborhoods are more expensive and which are cheaper. We found that Rolling Hills has an average price of $2667.6 which is the most expensive one in LA. Cudahy has an average price of $14 which is the cheapest one.
+
+    Then, we also would like to know the averge prices of all the room types. We found that entire home or apartment has the highest average price and shared room has the lowest average price. This result is quite intuitive.
+
+    We used tables to represent these data as information can be displayed in a clear and direct manner. 
+
+- Where are the most expensive and cheapest properties located on the map? 
+
+    Then We are curious about where are the most expensive properties located in LA. Since we have the latitude and longitude variables in our dataset, we think it would be good to plot out interactive maps that enable different levels of views by zooming in or out. Thus, we generated maps to show the most expensive Airbnbs priced at $800 and above. The result indicated that most of the expensive properties are concentrated in the Beverly Hills and West Hollywood neighborhoods. Also, some of them are scattered around the coastline. We are also curious about where are the cheapest properties located in LA so we plot out another map for those priced at $50 and below. It turns out that they are roughly evenly distributed on the map. 
+
+    We also displayed the top five most expensive properties and the top five cheapest properties using tables. One interesting fact we noticed was that the top five cheapest properties all had a listing price of 0. 
+
+- Where are the most and least popular/legit properties located on the map?
+
+    Furthermore, we want to know where are the most popular/legit properties(those with the most number of reviews) and the least popular/legit properties located. So again we plot out two maps to see the distributions. We noticed that the distribution of the most popular properties on map is similar to that of the most expensive properties. Also, the distribution of the least popular properties on map is similar to that of the cheapest properties. This indicates that it is likely that price is positively associated with number of reviews.
+
+    We also displayed the top five most popular properties and the top five least popular properties using tables.
+
+
+- Listing Price Prediction
+    
+    We finally deployed a Linear Regression model to predict the price with four different variables so that users can specify values for these variables in order to get an estimate.  These four variables are number of reviews, availability in one year, minimum nights, and calculated host listings count. 
+    
+    One alternative we considered was the Random Forest model, which is more likely to have a better-predicted result due to its complexity. However, it takes a long time to run so we finally decided to use Linear Regression to obtain a reasonably good prediction with high computational efficiency.
+
+
 
 ## Development
 
-TODO: **An overview of your development process.** Describe how the work was split among the team members. Include a commentary on the development process, including answers to the following questions: Roughly how much time did you spend developing your application (in people-hours)? What aspects took the most time?
+- We first tried to search for a good dataset for our project. Since we are about to take a trip to Los Angeles during Thanksgiving, we would like to know the Airbnb situation there before the trip. Also, this dataset is relatively clean with a small portion of n/a values and contains geographic features which are good for developing interative applications. **[3 hours (together)]**
+
+- We think it is important to determine the goals of this project before we start working on it. So, we set up a meeting, did some investigations on the  questions that might be of the most interest to the customers of Airbnb. **[1.5 hours (together)]**
+
+- We then carefully investigated the dataset and came up with our final research questions. We first brainstormed the questions separately, then combined our thoughts together. After that, we select certain useful features among all the columns that would help us answer these questions. Then we preprocessed the dataset by filtering out the nan values and removing columns that we are not interested in. We also did some feature engineering on the dataset, and in particular, we created one-hot encoding features from room_type and neighbourhood. **[4 hours (each)]**
+
+- Then, Xi did exploratory data analysis on the dataset, such as plotting out the distributions of certain variables. And Shengli plot out the maps to show the locations of the most expensive and cheapest properties. **[2 hours (1 hour each person)]**
+
+- We then together did the rest of the project to answer the remaining questions we proposed, such as obtaining the average prices of the top 5 most expensive and cheapest neighborhoods, and average prices by room type. **[2 hours (together)]**
+
+- We then engaged in a very intense discussion on which model to choose for price prediction. We initially thought about Random Forest, because we want to include different variable types in the predictors, including integers, floats, and OHE vectors. However, when we deployed it, it took around 3 minutes for our project to run, which is not very efficient for an interactive project. So, we decided to switch to another model which can fit the data more efficiently. Linear regression was our final choice. **[5 hours (together)]**
+
+- After finished coding, we wrote the whole writeup together and submitted it to Github. **[2 hours (together)]**
